1f43a26c58da339d6da39db8785c0ec15df70d60
alexandre <contact@alexandreconseil.fr>
2023-11-25 17:20:19 +0100

    Modified main.c

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		// We launch the REPL
+	} else if (argc == 2) {
+		// We need to interpret a file
+		// so we check whether the file exists
+	} else {
+		// An error in the CLI usage, so we use `cli/options/`
+		// to print the program usage
+	}
+
 	return EXIT_SUCCESS;
 }
